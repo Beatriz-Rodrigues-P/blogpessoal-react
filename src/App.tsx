36ea04d0d7f8306a0cd567597b83c60d195a062e
Beatriz <beatriz.rodrigues04@outlook.com>
2025-02-24 17:14:35 -0300

    Atualização da Home no App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
-import Home from "./pages/home/Home"
 import Login from './pages/login/Login'
 import Cadastro from './pages/cadastro/Cadastro'
 import { AuthProvider } from './contexts/AuthContext'
@@ -14,6 +13,7 @@ import FormPostagem from './components/postagens/formpostagem/FormPostagem'
 import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem'
 import Perfil from './pages/perfil/Perfil'
 import { ToastContainer } from 'react-toastify'
+import Home from './pages/home/Home'
 
 function App() {
   return (
