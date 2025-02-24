0633a6519bf10dda5efef068469bd9e33d53bb72
Beatriz <beatriz.rodrigues04@outlook.com>
2025-02-24 16:10:50 -0300

    Correção de erros para o deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home'
+import Home from "./pages/home/Home"
 import Login from './pages/login/Login'
 import Cadastro from './pages/cadastro/Cadastro'
 import { AuthProvider } from './contexts/AuthContext'
